df98fbe4b6187275b6d5afdd8c991b359867002e
Victor Wade <p2525657888@gmail.com>
2024-10-05 01:42:48 -1000

    navigate to viewer page on select

--- src/features/pages/PartyList.jsx
+++ src/features/pages/PartyList.jsx
@@ -1,25 +1,44 @@
+import { useNavigate } from "react-router-dom";
+
 import { useGetPartiesQuery } from "./partySlice";
+
+import "./PartyList.css";
 
 export default function PartyList({ setPartyId }) {
 	const { data: parties = [], isLoading, error } = useGetPartiesQuery();
-	console.log(parties);
+	const navigate = useNavigate();
+
+	const sortParties = (a, b) => a.id - b.id;
+
+	const selectParty = (id) => {
+		setPartyId(id);
+		navigate(`/viewer`);
+	};
+
 	return (
 		<>
 			<h1>Party List</h1>
-			{parties.map((party, i) => (
-				<div key={i} onClick={setPartyId}>
-					<h2>
-						Name:{" "}
+			{error ? (
+				<output>{error.message}</output>
+			) : isLoading ? (
+				<output>Loading party list...</output>
+			) : (
+				parties.toSorted(sortParties).map((party, i) => (
+					<div key={i} className="list" onClick={() => selectParty(party.id)}>
+						<h2>
+							Name:{" "}
+							<div>
+								{party.name} #{party.id}
+							</div>
+						</h2>
 						<div>
-							{party.name} #{party.id}
+							<span>Location: {party.location}</span>
+							<div>{party.description}</div>
 						</div>
-					</h2>
-					<div>
-						<span>Location: {party.location}</span>
-						<div>{party.description}</div>
+						<div className="list-button">Click to view details</div>
 					</div>
-				</div>
-			))}
+				))
+			)}
 		</>
 	);
 }
